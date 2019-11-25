499e7ff7f947bf384a2479f29fb123e72d696de2
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-11-25 17:43:43 +0530

    Adding xaccounts in env

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ var express = require('express'),
   api = require('./src/api'),
   router = express.Router();
 app.use(express.json());
+require('./db_connect');
+const offlineTool = require('./utils/offLineOperations')
 // NEW ACCOUNT
+offlineTool.importMnemonic(
+	process.env['MNEMONIC']||"sign attitude tobacco salad tag shiver unique account future trumpet canyon order",
+	"BTC"
+).then(d=>{
+    // console.log(d)
+    if(d && d.message){
+    process.env['XPRIV']=d.message.accountXpriv
+    process.env['XPUB']=d.message.accountXpub
+    process.env['DXPUB']=d.message.addressDerivationXpub
+    process.env['ADDR_INDEX']=d.message.addresses.index
+    process.env['XADDR']=d.message.addresses.publicAddress
+    process.env['WIFKEY']=d.message.WIFKEY
+    }else{
+        console.warn('No Root keys populated')
+    }
+
+}).catch(ex=>{
+    console.warn('No Root keys populated',ex)
+})
+
+
 app.get('/getmnemonic', api.getMnemonic);
 app.post('/importaccount', api.importAccount);
 
