348ac64fd4a85a033320b6b3bcff999b9d8fcf98
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-12-01 10:59:11 +0530

    integrating exchaneg apis

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   api = require('./src/api'),
+  exchange= require('./src/exchange')
   router = express.Router();
 app.use(express.json());
 require('./db_connect');
@@ -32,15 +33,21 @@ app.get('/getmnemonic', api.getMnemonic);
 app.post('/importaccount', api.importAccount);
 
 //BALANCE
-app.get('/balance', api.balance);
+app.get('/raw/balance', api.balance);
 
 // GENERATE ADDRESSES
 app.post('/generateAddress', api.generateAddress);
 
-// SENDING
-app.post('/sendtransaction/createTxn', api.createTxn);
-app.post('/sendtransaction/signTxn', api.signTxn);
-app.post('/broadcastTransaction',api.broadcastTransaction)
+// SENDING RAW
+app.post('/sendtransaction/raw/createTxn', api.createTxn);
+app.post('/sendtransaction/raw/signTxn', api.signTxn);
+app.post('/raw/broadcastTransaction',api.broadcastTransaction);
+
+app.get('/balance',exchange.getBalance);
+app.post('/send',exchange.createTrx);
+app.get('/trx',exchange.getTrxByAddress);
+
+
 // TRANSACTION STATUS
 app.get('/tx', api.tx);
 
