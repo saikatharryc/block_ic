756fbd97a0109dc9778c314e4b259c957468729f
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-12-02 22:44:29 +0530

    circular dep fix

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ var express = require('express'),
   router = express.Router();
 app.use(express.json());
 require('./db_connect');
-const offlineTool = require('./utils/offLineOperations')
+// const offlineTool = require('./utils/offLineOperations')
 // NEW ACCOUNT
 
 
-offlineTool.importMnemonic(
-	process.env['MNEMONIC']||"sign attitude tobacco salad tag shiver unique account future trumpet canyon order",
-	"BTC"
-).then(d=>{
-    // console.log(d)
-    if(d && d.message){
-    process.env['XPRIV']=d.message.accountXpriv
-    process.env['XPUB']=d.message.accountXpub
-    process.env['DXPUB']=d.message.addressDerivationXpub
-    process.env['ADDR_INDEX']=d.message.addresses.index
-    process.env['XADDR']=d.message.addresses.publicAddress
-    process.env['WIFKEY']=d.message.WIFKEY
-    }else{
-        console.warn('No Root keys populated')
-    }
+// offlineTool.importMnemonic(
+// 	process.env['MNEMONIC']||"sign attitude tobacco salad tag shiver unique account future trumpet canyon order",
+// 	"BTC"
+// ).then(d=>{
+//     // console.log(d)
+//     if(d && d.message){
+//     process.env['XPRIV']=d.message.accountXpriv
+//     process.env['XPUB']=d.message.accountXpub
+//     process.env['DXPUB']=d.message.addressDerivationXpub
+//     process.env['ADDR_INDEX']=d.message.addresses.index
+//     process.env['XADDR']=d.message.addresses.publicAddress
+//     process.env['WIFKEY']=d.message.WIFKEY
+//     }else{
+//         console.warn('No Root keys populated')
+//     }
 
-}).catch(ex=>{
-    console.warn('No Root keys populated',ex)
-})
+// }).catch(ex=>{
+//     console.warn('No Root keys populated',ex)
+// })
 
 
 app.get('/getmnemonic', api.getMnemonic);
